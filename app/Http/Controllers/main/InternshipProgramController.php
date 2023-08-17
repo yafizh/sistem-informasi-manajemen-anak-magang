4a511af9611ec5d9511efdbf61ca3999a03f64f6
yafizh <a.chairil.yafizh@gmail.com>
2023-08-17 20:11:43 +0700

    revisi sertifikat

--- app/Http/Controllers/main/InternshipProgramController.php
+++ app/Http/Controllers/main/InternshipProgramController.php
@@ -10,18 +10,46 @@ class InternshipProgramController extends Controller
 {
     public function certificate(Student $student)
     {
-        $evaluation = collect($student->internshipStudent->evaluation->only('attitude', 'discipline', 'diligence', 'independent_work', 'collaboration', 'accuracy', 'communication', 'creativity'));
-        $evaluation = round($evaluation->sum() / 8);
-        if ($evaluation > 90 && $evaluation <= 100)
-            $evaluation = 'A';
-        elseif ($evaluation > 70 && $evaluation <= 90)
-            $evaluation = 'B';
-        elseif ($evaluation > 60 && $evaluation <= 70)
-            $evaluation = 'C';
-        elseif ($evaluation > 50 && $evaluation <= 60)
-            $evaluation = 'D';
-        else
-            $evaluation = 'E';
+        $evaluation = $student->internshipStudent->evaluation->only('attitude', 'discipline', 'diligence', 'independent_work', 'collaboration', 'accuracy', 'communication', 'creativity');
+
+        $evalutationTable = [];
+        foreach ($evaluation as $key => $value) {
+            if ($key === 'attitude') {
+                $name = 'SIKAP';
+            } elseif ($key === 'discipline') {
+                $name = 'DISIPLIN';
+            } elseif ($key === 'diligence') {
+                $name = 'KETEKUNAN';
+            } elseif ($key === 'independent_work') {
+                $name = 'KERJA MANDIRI';
+            } elseif ($key === 'collaboration') {
+                $name = 'KERJA SAMA';
+            } elseif ($key === 'accuracy') {
+                $name = 'KETELITIAN';
+            } elseif ($key === 'communication') {
+                $name = 'KOMUNIKASI';
+            } elseif ($key === 'creativity') {
+                $name = 'KREATIFITAS';
+            }
+
+            if ($value > 90 && $value <= 100)
+                $description = 'A';
+            elseif ($value > 70 && $value <= 90)
+                $description = 'B';
+            elseif ($value > 60 && $value <= 70)
+                $description = 'C';
+            elseif ($value > 50 && $value <= 60)
+                $description = 'D';
+            else
+                $description = 'E';
+
+            $evalutationTable[] = [
+                'name' => $name,
+                'value' => $value,
+                'description' => $description
+            ];
+        }
+
 
         $start_date = new Carbon($student->internshipStudent->internshipProgram->start_date);
         $end_date = new Carbon($student->internshipStudent->internshipProgram->end_date);
@@ -32,6 +60,7 @@ class InternshipProgramController extends Controller
             'student_status' => $student->student_status,
             'start_date' => $start_date->day . " " . $start_date->locale('ID')->getTranslatedMonthName() . " " . $start_date->year,
             'end_date' => $end_date->day . " " . $end_date->locale('ID')->getTranslatedMonthName() . " " . $end_date->year,
+            'evalutationTable' => $evalutationTable
         ]);
     }
 }
--- resources/views/certificates/certificate.blade.php
+++ resources/views/certificates/certificate.blade.php
@@ -1,16 +1,34 @@
 <html>
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style type="text/css">
         @page {
             size: A4 landscape;
         }
 
+        .pagebreak {
+            page-break-before: always;
+        }
+
+
         @media screen,
         print {
 
+
             body,
             html {
+                -webkit-print-color-adjust: exact !important;
+                print-color-adjust: exact !important;
+                margin: 0;
+                padding: 0;
+                /* height: 100vh; */
+                position: relative;
+                overflow: hidden;
+            }
+
+            #certificate {
                 -webkit-print-color-adjust: exact !important;
                 print-color-adjust: exact !important;
                 margin: 0;
@@ -59,11 +77,6 @@
         .certificate {
             font-size: 3rem;
             font-weight: bold;
-            margin-bottom: 2rem;
-        }
-
-        .to {
-            margin-bottom: 2rem;
         }
 
         .name {
@@ -91,44 +104,83 @@
 </head>
 
 <body>
-    <div class="square" style="top: -220; left: -250">
-        <div class="square-with-border" style="top: -120; left: -60"></div>
-        <div class="square-with-border" style="top: 450; left: -40"></div>
-    </div>
-    <div class="bar" style="bottom: -100; left: -70"></div>
-    <div class="bar" style="bottom: -300; left: -40"></div>
-
-    <main>
-        <div style="width: 10rem; margin: 1rem auto">
-            <img src="https://www.telkom.co.id/images/bumper_telkom_indonesia.webp" style="width: 100%" />
+    <div id="certificate" class="p-0 m-0">
+        <div class="square" style="top: -220; left: -250">
+            <div class="square-with-border" style="top: -120; left: -60"></div>
+            <div class="square-with-border" style="top: 450; left: -40"></div>
         </div>
-        <img src="{{ asset('assets/img/badge.png') }}"
-            style="width: 14rem; position: absolute; top: 40; right: 100; z-index: 99;" />
-        <div class="certificate">SERTIFIKAT</div>
-        <div>dengan nilai</div>
-        <h1>{{ $evaluation }}</h1>
-        <div class="to">diberikan kepada</div>
-        <div class="name">{{ $name }}</div>
-        <div class="id_number">{{ $student_status == 1 ? 'NIS/NISN' : 'NIM/NPM' }}. {{ $id_number }}</div>
-        <div class="reason">
-            telah melaksanakan praktik magang di PT. Telekomunikasi Indonesia
-            (Persero)
-        </div>
-        <div class="at">Periode Magang {{ $start_date }} - {{ $end_date }}</div>
-        <div>Mengetahui,</div>
-        <div>HEAD OF REPRESENTATIVE OFFICE MARABAHAN</div>
-        <br /><br /><br /><br /><br /><br />
-        <div class="sign-line"></div>
-        <div>DIMITRI ERLANGGA</div>
-        <div>NIK. 960277</div>
-    </main>
+        <div class="bar" style="bottom: -100; left: -70"></div>
+        <div class="bar" style="bottom: -300; left: -40"></div>
 
-    <div class="square" style="bottom: -220; right: -250">
-        <div class="square-with-border" style="bottom: -120; right: -60"></div>
-        <div class="square-with-border" style="bottom: 450; right: -40"></div>
+        <main>
+            <div style="width: 10rem; margin: 1rem auto">
+                <img src="https://www.telkom.co.id/images/bumper_telkom_indonesia.webp" style="width: 100%" />
+            </div>
+            <img src="{{ asset('assets/img/badge.png') }}"
+                style="width: 14rem; position: absolute; top: 40; right: 100; z-index: 99;" />
+            <div class="certificate">SERTIFIKAT</div>
+            <div class="to">diberikan kepada</div>
+            <div class="name">{{ $name }}</div>
+            <div class="id_number">{{ $student_status == 1 ? 'NIS/NISN' : 'NIM/NPM' }}. {{ $id_number }}</div>
+            <div class="reason">
+                telah melaksanakan praktik magang di PT. Telekomunikasi Indonesia
+                (Persero)
+            </div>
+            <div class="at">Periode Magang {{ $start_date }} - {{ $end_date }}</div>
+            <div>Mengetahui,</div>
+            <div>HEAD OF REPRESENTATIVE OFFICE MARABAHAN</div>
+            <br /><br /><br /><br /><br /><br />
+            <div class="sign-line"></div>
+            <div>DIMITRI ERLANGGA</div>
+            <div>NIK. 960277</div>
+        </main>
+
+        <div class="square" style="bottom: -220; right: -250">
+            <div class="square-with-border" style="bottom: -120; right: -60"></div>
+            <div class="square-with-border" style="bottom: 450; right: -40"></div>
+        </div>
+        <div class="bar" style="top: -100; right: -70"></div>
+        <div class="bar" style="top: -300; right: -40"></div>
     </div>
-    <div class="bar" style="top: -100; right: -70"></div>
-    <div class="bar" style="top: -300; right: -40"></div>
+
+    <div class="pagebreak"></div>
+    <div class="container p-5">
+        <h4 class="text-center mb-5">DAFTAR PENILAIAN</h4>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Aspek Penilaian</th>
+                    <th class="text-center">Skor Nilai</th>
+                    <th class="text-center">Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $total = 0;
+                @endphp
+                @foreach ($evalutationTable as $value)
+                    <tr>
+                        <td class="text-center">{{ $loop->iteration }}</td>
+                        <td class="text-center">{{ $value['name'] }}</td>
+                        <td class="text-center">{{ $value['value'] }}</td>
+                        <td class="text-center">{{ $value['description'] }}</td>
+                    </tr>
+                    @php
+                        $total += $value['value'];
+                    @endphp
+                @endforeach
+                <tr>
+                    <td colspan="2">Total</td>
+                    <td colspan="2" class="text-center"><strong>{{ $total }}</strong></td>
+                </tr>
+                <tr>
+                    <td colspan="2">Rata - Rata</td>
+                    <td colspan="2" class="text-center"><strong>{{ round($total / 8, 2) }}</strong></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <script>
         window.print();
     </script>
