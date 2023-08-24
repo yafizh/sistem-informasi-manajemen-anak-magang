18b24d9c6df1ee34e7d3c73ececd36b8572251dc
yafizh <a.chairil.yafizh@gmail.com>
2023-08-24 07:09:10 +0700

    update header report

--- resources/views/dashboard/admin/prints/header.blade.php
+++ resources/views/dashboard/admin/prints/header.blade.php
@@ -1,9 +1,9 @@
 <header class="text-center p-4">
     <img src="/assets/img/logo/logo1.png" alt="Logo" width="110" style="position: absolute; left: 30px;">
     <h4>PT TELKOM</h4>
-    Jl. Cisirung No.259/99, Cangkuang Wetan, Kec. Dayeuhkolot,
+    Kota Banjarbaru Jl. Pangeran HM Noor, Kemuning, 
     <br>
-    Kabupaten Bandung, Jawa Barat 40256
+    Kec. Banjarbaru Selatan, Kota Banjar Baru, Kalimantan Selatan 70714
 </header>
 <div class="d-flex flex-column justify-content-center w-100">
     <div style="width: 100%; border-top: 3px solid black;"></div>
